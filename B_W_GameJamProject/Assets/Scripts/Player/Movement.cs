280e60e296d523569b2c37354e033425da492226
MarcoM-Developer <marcomingiardi.pp@gmail.com>
2022-03-28 20:18:15 +0200

    Just a backup commit

--- B_W_GameJamProject/Assets/Scripts/Player/Movement.cs
+++ B_W_GameJamProject/Assets/Scripts/Player/Movement.cs
@@ -6,11 +6,16 @@ public class Movement : MonoBehaviour
 {
     [Header("Velocity")]
 
+    [SerializeField] private bool isMoving;
+    [SerializeField] private bool isAtMaxSpeed = false;
+
     [SerializeField] private FloatReference maxSpeed;           //serialize field takes reference from inspector and only if the component is on the same object
     [SerializeField] private FloatReference acceleration;              //otherwise I use getComponent method with GameObject.Find()
     [SerializeField] private FloatReference deceleration;
-    [SerializeField] private bool isAtMaxSpeed = false;
-    [SerializeField] private bool flag = false;
+    [SerializeField] private FloatReference debugTresholdForFalseMaxSpeed;
+    
+    [SerializeField] private FloatReference remainingTime;
+    
 
     [Header("Components")]
 
@@ -28,90 +33,78 @@ public class Movement : MonoBehaviour
     private void Update()
     {
         directionVector = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
-        if (directionVector.x < 0)
-        {
 
+        if (directionVector.x > 0 && !isAtMaxSpeed)
+        {
+            isMoving = true;
         }
+        if (directionVector.x < 0 && !isAtMaxSpeed)
+        {
+            isMoving = true;
+        }
+        if (directionVector.x > 0 && playerRigidBody.velocity.x < 0)
+        {
+            isMoving = false;
+            isAtMaxSpeed = false;
+        }
+        if (directionVector.x < 0 && playerRigidBody.velocity.x > 0)
+        {
+            isMoving = false;
+            isAtMaxSpeed = false;
+        }
+        if (directionVector.x == 0)
+        {
+            isMoving = false;
+            isAtMaxSpeed = false;
+        }
+
+        CheckIfMaxSpeedReached();
+
+        FlipGameObjectLeftOrRight();
+        
     }
 
     private void FixedUpdate()
     {
-        PlayerAddForceMovementWithRightPhysics();
-        //PlayerVelocityMovement();
+        Move();
+        StopMove();
     }
 
-    private void PlayerAddForceMovementWithRightPhysics()
+    private void Move()
     {
-        CheckIfPlayerHasReachedMaxSpeed();
-
-        MovePlayerInADirectionIfPressesInputsOrStopItIfPressesNothing();
-
-        ReduceVelocityWhenPlayerIsMovingInADirectionAndSuddenlyChangesIt();
-
-        IfPlayerReachesMaxSpeedOnXStopAddingForceAndSetVelocityOnlyOnce();
-
-    }
-
-    /*private void PlayerVelocityMovement()
-    {
-        playerRigidBody.velocity = new Vector2(directionVector.x * maxSpeed.Value, playerRigidBody.velocity.y);
-    }*/
-
-    private void CheckIfPlayerHasReachedMaxSpeed()
-    {
-        if (Mathf.Abs(playerRigidBody.velocity.x) >= maxSpeed.Value)
+        if (isMoving && !isAtMaxSpeed)
         {
+            playerRigidBody.AddForce(directionVector * acceleration.Value);
+        }
+    }
+
+    private void StopMove()
+    {
+        if (!isMoving)
+        {
+            playerRigidBody.velocity = new Vector2(0, playerRigidBody.velocity.y);
+        }
+    }
+
+    private void CheckIfMaxSpeedReached()
+    {
+        if (Mathf.Abs(playerRigidBody.velocity.x) >= maxSpeed.Value && !isAtMaxSpeed)
+        {
+            playerRigidBody.velocity = new Vector2(maxSpeed.Value * directionVector.x, playerRigidBody.velocity.y);
             isAtMaxSpeed = true;
         }
-        else
-        {
-            isAtMaxSpeed = false;
-        }
+
     }
 
-    private void IfPlayerReachesMaxSpeedOnXStopAddingForceAndSetVelocityOnlyOnce()
-    {
-        if (isAtMaxSpeed && !flag)
-        {
-            flag = true;
-            playerRigidBody.velocity = new Vector2(Mathf.Sign(playerRigidBody.velocity.x) * maxSpeed.Value, playerRigidBody.velocity.y);
-        }
-        else if (isAtMaxSpeed && directionVector.x == 0)
-        {
-            flag = false;
-            playerRigidBody.velocity = new Vector2(0, playerRigidBody.velocity.y);
-        }
-    }
-
-    private void MovePlayerInADirectionIfPressesInputsOrStopItIfPressesNothing()
-    {
-        if (directionVector.x != 0)
-        {
-            playerRigidBody.AddForce(directionVector * acceleration.Value, ForceMode2D.Force);
-        }
-        else
-        {
-            playerRigidBody.velocity = new Vector2(0, playerRigidBody.velocity.y);
-        }
-    }
-
-    private void ReduceVelocityWhenPlayerIsMovingInADirectionAndSuddenlyChangesIt()
+    private void FlipGameObjectLeftOrRight()
     {
         if (directionVector.x < 0)
         {
-            if (playerRigidBody.velocity.x > 0)
-            {
-                playerRigidBody.velocity = new Vector2(0, playerRigidBody.velocity.y);
-            }
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 180, transform.rotation.eulerAngles.z);
         }
-        if (directionVector.x > 0)
+        else if (directionVector.x > 0)
         {
-            if (playerRigidBody.velocity.x < 0)
-            {
-                playerRigidBody.velocity = new Vector2(0, playerRigidBody.velocity.y);
-            }
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0, transform.rotation.eulerAngles.z);
         }
     }
-
-    
 }
