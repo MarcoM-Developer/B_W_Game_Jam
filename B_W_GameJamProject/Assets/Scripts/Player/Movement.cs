fac4db1a36094ed27984f7ef300265f65e3af8e4
MarcoM-Developer <marcomingiardi.pp@gmail.com>
2022-03-28 23:04:30 +0200

    Player Movement Trough Velocity

--- B_W_GameJamProject/Assets/Scripts/Player/Movement.cs
+++ B_W_GameJamProject/Assets/Scripts/Player/Movement.cs
@@ -8,6 +8,7 @@ public class Movement : MonoBehaviour
 
     [SerializeField] private bool isMoving;
     [SerializeField] private bool isAtMaxSpeed = false;
+    [SerializeField] private bool isTimerActive = true;
 
     [SerializeField] private FloatReference maxSpeed;           //serialize field takes reference from inspector and only if the component is on the same object
     [SerializeField] private FloatReference acceleration;              //otherwise I use getComponent method with GameObject.Find()
@@ -15,7 +16,8 @@ public class Movement : MonoBehaviour
     [SerializeField] private FloatReference debugTresholdForFalseMaxSpeed;
     
     [SerializeField] private FloatReference remainingTime;
-    
+    [SerializeField] private FloatReference currentTime;
+
 
     [Header("Components")]
 
@@ -26,7 +28,7 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-
+        currentTime.Value = 0;
     }
 
     // Update is called once per frame
@@ -34,63 +36,18 @@ public class Movement : MonoBehaviour
     {
         directionVector = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
 
-       /*playerRigidBody.velocity = new Vector2(directionVector.x * acceleration.Value, playerRigidBody.velocity.y);
-    }*/
-        if (directionVector.x > 0 || directionVector.x < 0)
-        {
-            isMoving = true;
-        }
-        else if((directionVector.x > 0 && playerRigidBody.velocity.x < 0) || (directionVector.x < 0 && playerRigidBody.velocity.x > 0))
-        {
-            isMoving = false;
-            isAtMaxSpeed = false;
-            playerRigidBody.velocity = new Vector2(0, playerRigidBody.velocity.y);
-        }
-        else if (directionVector.x == 0)
-        {
-            isMoving = false;
-            isAtMaxSpeed = false;
-        }
+        playerRigidBody.velocity = new Vector2(directionVector.x * acceleration.Value, playerRigidBody.velocity.y);
 
-        CheckIfMaxSpeedReached();
-
-        FlipGameObjectLeftOrRight();
-        
+        FlipObjectHorizontally();        
     }
+    
 
     private void FixedUpdate()
     {
-        Move();
-        StopMove();
+       
     }
 
-    private void Move()
-    {
-        if (isMoving && !isAtMaxSpeed)
-        {
-            playerRigidBody.AddForce(directionVector * acceleration.Value);
-        }
-    }
-
-    private void StopMove()
-    {
-        if (!isMoving)
-        {
-            playerRigidBody.velocity = new Vector2(0, playerRigidBody.velocity.y);
-        }
-    }
-
-    private void CheckIfMaxSpeedReached()
-    {
-        if (Mathf.Abs(playerRigidBody.velocity.x) >= maxSpeed.Value && !isAtMaxSpeed)
-        {
-            playerRigidBody.velocity = new Vector2(maxSpeed.Value * directionVector.x, playerRigidBody.velocity.y);
-            isAtMaxSpeed = true;
-        }
-
-    }
-
-    private void FlipGameObjectLeftOrRight()
+    private void FlipObjectHorizontally()
     {
         if (directionVector.x < 0)
         {
@@ -101,6 +58,4 @@ public class Movement : MonoBehaviour
             transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0, transform.rotation.eulerAngles.z);
         }
     }
-
-    
 }
