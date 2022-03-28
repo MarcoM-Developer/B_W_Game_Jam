d74b62b7a97d634c3d672b8f9de7ce774fb25f43
MarcoM-Developer <marcomingiardi.pp@gmail.com>
2022-03-28 15:21:22 +0200

    Player movement with right physics
    Now player movement is done in a way that considers the unity physics system in a proper way.

--- B_W_GameJamProject/Assets/Scripts/Player/Movement.cs
+++ B_W_GameJamProject/Assets/Scripts/Player/Movement.cs
@@ -4,24 +4,114 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private FloatReference speed;              //serialize field takes reference from inspector and only if the component is on the same object
-    [SerializeField] private Rigidbody2D playerRigidBody;       //otherwise I use getComponent method with GameObject.Find()
-    private Vector2 movementVector;
+    [Header("Velocity")]
+
+    [SerializeField] private FloatReference maxSpeed;           //serialize field takes reference from inspector and only if the component is on the same object
+    [SerializeField] private FloatReference acceleration;              //otherwise I use getComponent method with GameObject.Find()
+    [SerializeField] private FloatReference deceleration;
+    [SerializeField] private bool isAtMaxSpeed = false;
+    [SerializeField] private bool flag = false;
+
+    [Header("Components")]
+
+    [SerializeField] private Rigidbody2D playerRigidBody;
+
+    private Vector2 directionVector;
 
     // Start is called before the first frame update
     private void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     private void Update()
     {
-        movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        directionVector = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        if (directionVector.x < 0)
+        {
+
+        }
     }
 
     private void FixedUpdate()
     {
-        playerRigidBody.velocity = new Vector2(movementVector.x * speed.Value * Time.deltaTime , playerRigidBody.velocity.y);
+        PlayerAddForceMovementWithRightPhysics();
+        //PlayerVelocityMovement();
     }
+
+    private void PlayerAddForceMovementWithRightPhysics()
+    {
+        CheckIfPlayerHasReachedMaxSpeed();
+
+        MovePlayerInADirectionIfPressesInputsOrStopItIfPressesNothing();
+
+        ReduceVelocityWhenPlayerIsMovingInADirectionAndSuddenlyChangesIt();
+
+        IfPlayerReachesMaxSpeedOnXStopAddingForceAndSetVelocityOnlyOnce();
+
+    }
+
+    /*private void PlayerVelocityMovement()
+    {
+        playerRigidBody.velocity = new Vector2(directionVector.x * maxSpeed.Value, playerRigidBody.velocity.y);
+    }*/
+
+    private void CheckIfPlayerHasReachedMaxSpeed()
+    {
+        if (Mathf.Abs(playerRigidBody.velocity.x) >= maxSpeed.Value)
+        {
+            isAtMaxSpeed = true;
+        }
+        else
+        {
+            isAtMaxSpeed = false;
+        }
+    }
+
+    private void IfPlayerReachesMaxSpeedOnXStopAddingForceAndSetVelocityOnlyOnce()
+    {
+        if (isAtMaxSpeed && !flag)
+        {
+            flag = true;
+            playerRigidBody.velocity = new Vector2(Mathf.Sign(playerRigidBody.velocity.x) * maxSpeed.Value, playerRigidBody.velocity.y);
+        }
+        else if (isAtMaxSpeed && directionVector.x == 0)
+        {
+            flag = false;
+            playerRigidBody.velocity = new Vector2(0, playerRigidBody.velocity.y);
+        }
+    }
+
+    private void MovePlayerInADirectionIfPressesInputsOrStopItIfPressesNothing()
+    {
+        if (directionVector.x != 0)
+        {
+            playerRigidBody.AddForce(directionVector * acceleration.Value, ForceMode2D.Force);
+        }
+        else
+        {
+            playerRigidBody.velocity = new Vector2(0, playerRigidBody.velocity.y);
+        }
+    }
+
+    private void ReduceVelocityWhenPlayerIsMovingInADirectionAndSuddenlyChangesIt()
+    {
+        if (directionVector.x < 0)
+        {
+            if (playerRigidBody.velocity.x > 0)
+            {
+                playerRigidBody.velocity = new Vector2(0, playerRigidBody.velocity.y);
+            }
+        }
+        if (directionVector.x > 0)
+        {
+            if (playerRigidBody.velocity.x < 0)
+            {
+                playerRigidBody.velocity = new Vector2(0, playerRigidBody.velocity.y);
+            }
+        }
+    }
+
+    
 }
