2d64a5754c63f1d2df62be0f21a09c6b1cafe855
Vojta Havlicek <vojta.havlicek@gmail.com>
2022-04-05 00:38:35 -0400

    Wiring

--- B_W_GameJamProject/Assets/Scripts/GridManager.cs
+++ B_W_GameJamProject/Assets/Scripts/GridManager.cs
@@ -12,18 +12,50 @@ public class GridManager : MonoBehaviour
     [SerializeField]
     private Tilemap whiteTileMap;
 
+    [SerializeField]
+    private List<TileData> tileDatas; // Load tile data here.
+    // TODO: add explicit reference to white and black walls
+
+    private Dictionary<TileBase, TileData> dataFromTiles;
+
+    private List<Vector3Int> activeWires = new List<Vector3Int>();
+
+
+    // Populate the dataFromTiles
+    private void Awake()
+	{
+       
+        dataFromTiles = new Dictionary<TileBase, TileData>();
+
+        foreach(var tileData in tileDatas)
+		{
+           
+                if (tileData.tile != null)
+                {
+                    Debug.Log("Nonnull tile added");
+
+                    dataFromTiles.Add(tileData.tile, tileData);
+                }
+			
+		}
+	}
+
+
+
     // Setup the dual map?
     void Start()
-	{
+    {
 
-	}
+    }
 
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-		{
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Test the activation
+
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector3Int blackTilePosition = blackTileMap.WorldToCell(mousePosition);
             Vector3Int whiteTilePosition = whiteTileMap.WorldToCell(mousePosition);
@@ -31,17 +63,25 @@ public class GridManager : MonoBehaviour
             TileBase blackTile = blackTileMap.GetTile(blackTilePosition);
             TileBase whiteTile = whiteTileMap.GetTile(whiteTilePosition);
 
+            
+
             if (blackTile != null)
             {
-                Debug.Log("Tile: " + blackTile.name);
+                
+                Debug.Log("Black Tile: " + blackTile.name);
+
+                if (dataFromTiles.ContainsKey(blackTile)) {
+                    Debug.Log("Propagates?: " + dataFromTiles[blackTile].propagatesSignal);
+                }
+                Spread(blackTilePosition);
             }
 
             if (whiteTile != null)
-			{
-                Debug.Log("Tile: " + whiteTile.name);
-			}
+            {
+                Debug.Log("Black Tile: " + whiteTile.name);
+            }
 
-            Spread(blackTilePosition);
+
 
         }
     }
@@ -60,15 +100,35 @@ public class GridManager : MonoBehaviour
             }
         }
 
-          
+
         void TryToSpreadTile(Vector3Int position)
         {
             TileBase tile = blackTileMap.GetTile(position);
 
-            if (tile != null && tile.name=="Checkerboard")
+            if (tile != null)
             {
-                blackTileMap.SetTile(position, null);
-                Spread(position);
+                switch (tile.name)
+                {
+                    case "Checkerboard":
+                        blackTileMap.SetTile(position, null);
+
+                        // HACK below!!! I don't know how to fetch the tile otherwise :)
+                        //whiteTile = whiteTileMap.GetTile();
+
+                        //whiteTileMap.SetTile(position, ); // Do the positions match?
+                        Spread(position);
+                        break;
+
+                    case "Wire":
+                        if (!activeWires.Contains(position))
+						{
+                            activeWires.Add(position);
+                            Spread(position);
+                        }
+                        
+                        break;
+
+                }
             }
         }
     }
--- B_W_GameJamProject/Assets/Scripts/TileData.cs
+++ B_W_GameJamProject/Assets/Scripts/TileData.cs
@@ -6,9 +6,20 @@ using UnityEngine.Tilemaps;
 [CreateAssetMenu]
 public class TileData : ScriptableObject
 {
-    public TileBase[] tiles;
+    public const string BLACKWIRE = "blackWire";
+    public const string WHITEWIRE = "whiteWire";
+    public const string CHECKERBOARD = "checkerboard";
+    public const string TEMPWHITEWALL = "tempWhiteWall";
+    public const string TEMPBLACKWALL = "tempBlackWall";
 
-    public bool canBurn;
-    public float spreadChance, spreadInterval, burnTime;
+    public static readonly string[] TYPES = { BLACKWIRE, WHITEWIRE, CHECKERBOARD, TEMPBLACKWALL, TEMPWHITEWALL };
+
+    public bool propagatesSignal = true; // Does the tile send signal?
+    public string type = BLACKWIRE;
+
+    public TileBase tile;
+
+    public bool isTemp;
+    //public float spreadChance, spreadInterval, burnTime;
 }
 
